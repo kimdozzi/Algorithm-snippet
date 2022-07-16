5b8e26535a3480c7939d21d1ae8ba37e1bd711c3
DoHyung Kim <neo5188@gmail.com>
2022-07-16 16:37:27 +0900

    added 1 case.

--- Python/inputType.py
+++ Python/inputType.py
@@ -3,3 +3,9 @@ n = int(input())
 lst = list(map(int, str(n)))
 
 lst = [int(i) for i in str(int(input()))]
+
+
+# 각 문자 사이에 공백이 없는 문자들을 입력받아 2차원 배열로 만들기
+matrix = []
+for i in range(n):
+    matrix.append([s for s in (input().rstrip())])
